5bed3bd8360492832d8f86c44a7da85f638d945f
younha00 <ayeon0@office.skhu.ac.kr>
2024-12-24 04:29:07 +0900

    ✨ [#4] Class-validator, Class-Transformer 추가
    DTO에 validation 추가 완료

--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,10 +1,13 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, Length } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
   @Field()
+  @IsEmail()
   email: string;
 
   @Field()
+  @Length(8, 20)
   password: string;
 }
