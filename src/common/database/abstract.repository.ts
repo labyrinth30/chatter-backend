f92f2e6b0a58e389584d85e7f489168019be955c
younha00 <ayeon0@office.skhu.ac.kr>
2024-12-24 02:10:06 +0900

    ✨ Feat: GraphQL에 맞게 엔티티 변경, query 메소드 생성
    Document -> Entity로 변경 및 _id 타입 명시

--- src/common/database/abstract.repository.ts
+++ src/common/database/abstract.repository.ts
@@ -1,25 +1,24 @@
-import { AbstractDocument } from './abstract.schema';
+import { AbstractEntity } from './abstract.entity';
 import { Logger, NotFoundException } from '@nestjs/common';
 import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
-import { filter } from 'rxjs';
 
-export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+export abstract class AbstractRepository<T extends AbstractEntity> {
   protected abstract readonly logger: Logger;
 
-  constructor(protected readonly model: Model<TDocument>) {}
+  constructor(protected readonly model: Model<T>) {}
 
-  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+  async create(document: Omit<T, '_id'>): Promise<T> {
     const createdDocument = new this.model({
       ...document,
       _id: new Types.ObjectId(),
     });
-    // as known을 사용함으로서 unknown에서 TDocument로 변환하기 전에 타입 체크를 요구하므로,
+    // as known을 사용함으로서 unknown에서 T로 변환하기 전에 타입 체크를 요구하므로,
     // 타입 불일치로 인한 오류를 미리 방지할 수 있다.
-    return (await createdDocument.save()).toJSON() as unknown as TDocument;
+    return (await createdDocument.save()).toJSON() as unknown as T;
   }
 
-  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
-    const document = await this.model.findOne(filterQuery).lean<TDocument>();
+  async findOne(filterQuery: FilterQuery<T>): Promise<T> {
+    const document = await this.model.findOne(filterQuery).lean<T>();
     if (!document) {
       this.logger.warn(`Document not found with filterQuery`, filterQuery);
       throw new NotFoundException(
@@ -30,14 +29,14 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   }
 
   async findOneAndUpdate(
-    filterQuery: FilterQuery<TDocument>,
-    update: UpdateQuery<TDocument>,
-  ): Promise<TDocument> {
+    filterQuery: FilterQuery<T>,
+    update: UpdateQuery<T>,
+  ): Promise<T> {
     const document = await this.model
       .findOneAndUpdate(filterQuery, update, {
         new: true,
       })
-      .lean<TDocument>();
+      .lean<T>();
     if (!document) {
       this.logger.warn(`Document not found with filterQuery`, filterQuery);
       throw new NotFoundException(
@@ -47,8 +46,8 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
     return document;
   }
 
-  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
-    const document = await this.model.find(filterQuery).lean<TDocument[]>();
+  async find(filterQuery: FilterQuery<T>): Promise<T[]> {
+    const document = await this.model.find(filterQuery).lean<T[]>();
     if (!document) {
       this.logger.warn(`Document not found with filterQuery`, filterQuery);
       throw new NotFoundException(
@@ -58,9 +57,7 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
     return document;
   }
 
-  async findOneAndDelete(
-    filterQuery: FilterQuery<TDocument>,
-  ): Promise<TDocument> {
-    return this.model.findOneAndDelete(filterQuery).lean<TDocument>();
+  async findOneAndDelete(filterQuery: FilterQuery<T>): Promise<T> {
+    return this.model.findOneAndDelete(filterQuery).lean<T>();
   }
 }
---
+++ src/users/users.repository.ts
@@ -0,0 +1,13 @@
+import { AbstractRepository } from '../common/database/abstract.repository';
+import { User } from './entities/user.entity';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+@Injectable()
+export class UsersRepository extends AbstractRepository<User> {
+  protected readonly logger = new Logger(UsersRepository.name);
+
+  constructor(@InjectModel(User.name) userModel: Model<User>) {
+    super(userModel);
+  }
+}
